e6104e6e7a4e67b841401e86982451054c61ad1a
hjz112 <78725323+hjz112@users.noreply.github.com>
2021-02-09 11:40:09 +0800

    1.1
    You can choose the number of letters
    Optimized the way of outputting results

--- Password.py
+++ Password.py
@@ -5,7 +5,6 @@ from easygui.boxes.choice_box import choicebox
 
 
 answer=ea.choicebox(msg="Password Numbers",title="Select",choices=["6","8","10"])
-
 if answer == "6":
     a=random.randint(100000,999999)
     b=random.randint(100000,999999)
@@ -39,14 +38,24 @@ else:
     h=random.randint(100000,9999999999)
     i=random.randint(100000,9999999999)
     j=random.randint(100000,9999999999)
-ab=string.ascii_lowercase
-abc=random.choice(ab)
-abs=string.ascii_lowercase
-abcs=random.choice(abs)
-abd=string.ascii_lowercase
-abcd=random.choice(abd)
 
-result = abc + abcs + abcd + str((a+b+c+d+e+f+g+h+i+j)/10)
+letter=ea.choicebox(msg='Password letter',title="Select",choices=["2",'3','4'])
+letter1=string.ascii_lowercase
+l1=random.choice(letter1)
+letter2=string.ascii_lowercase
+l2=random.choice(letter2)
+letter3=string.ascii_lowercase
+l3=random.choice(letter3)
+letter4=string.ascii_lowercase
+l4=random.choice(letter4)
+if letter == '2':
+    result= l1 + l2 + str((a+b+c+d+e+f+g+h+i+j)/10)
+elif letter == '3':
+    result= l1 + l2 + l3 + str((a+b+c+d+e+f+g+h+i+j)/10)
+else:
+    result= l1 + l2 + l3 + l4 + str((a+b+c+d+e+f+g+h+i+j)/10)
 
 
-print (result)
+
+
+ea.msgbox(result,title='result')
